59971c419c56f2f7b180a5f13097b613660e9253
cwurst <cwurst@uoregon.edu>
2025-04-17 12:54:49 -0700

    Add lexical analysis testing

---
+++ src/scanner.test.ts
@@ -0,0 +1,87 @@
+import { test, fc } from '@fast-check/vitest';
+import { expect } from 'vitest';
+import { scan, Token, TokenKind, Symbol} from './scanner';
+
+// Note: These tests are brittle wrt dummy pre- and post-tags
+
+const symbolArb = fc.constantFrom(
+    ...Object.values(Symbol).map((s) => s as string)
+);
+const sourceArb = fc.array(symbolArb).map((l) => l.join(''));
+
+test.prop([sourceArb])('should preserve lexemes', (source) => {
+    expect(scan(source).map((t) => t.lexeme).join('')).toBe(source);
+});
+
+test.prop([sourceArb.filter((s) => isPureText(s))])(
+    'should scan symbols outside of a tag as text', 
+    (source) => { 
+        let tokens = scan(source);
+        expect(tokens.length).toBe(3);
+        expectDummy(tokens[0], tokens[2]);
+        expect(tokens[1].kind == TokenKind.Text);
+    }
+);
+
+test('should scan an empty source to a list of two empty text tokens and an ' + 
+    'EOF token', () => {
+        let tokens = scan('');
+        expect(tokens.length).toBe(3);
+        expectDummy(tokens[0], tokens[2])
+        expect(isEmpty(tokens[1]));
+    }
+);
+
+test('should scan all kinds of token', () => {
+    let source = 'ipsum' + 
+        Symbol.DoubleLeftBracket + 
+        Symbol.RightBracket + 
+        Symbol.Tilde + 
+        Symbol.Colon +
+        Symbol.Comma +
+        Symbol.If + ' ' +
+        Symbol.Else + ' ' + 
+        Symbol.Tin + ' ' + 
+        'identifier' + 
+        Symbol.DoubleRightBracket;
+    let tokens = scan(source);
+    let expectedKinds:TokenKind[] = [
+        TokenKind.Text,
+        TokenKind.Text,
+        TokenKind.TagOpen,
+        TokenKind.Bad,
+        TokenKind.Tilde,
+        TokenKind.Colon,
+        TokenKind.Comma,
+        TokenKind.If,
+        TokenKind.Whitespace,
+        TokenKind.Else,
+        TokenKind.Whitespace,
+        TokenKind.Tin,
+        TokenKind.Whitespace,
+        TokenKind.Identifier,
+        TokenKind.TagClose,
+        TokenKind.Text,
+        TokenKind.EOF
+    ]
+    expect(tokens.length).toBe(expectedKinds.length);
+    let length = Math.min(tokens.length, expectedKinds.length);
+    for (let i = 0; i < length; i++)
+        expect(tokens[i].kind).toBe(expectedKinds[i]);
+});
+
+/**
+ * Expect each scan to start with an empty text token and end with an eof.
+ */
+function expectDummy(firstText: Token, eof: Token) {
+    expect(isEmpty(firstText));
+    expect(eof.kind == TokenKind.EOF);
+}
+
+function isEmpty(t: Token) { 
+    t.kind == TokenKind.Text && t.lexeme == '';
+}
+
+function isPureText(s: string) {
+    return !s.includes(Symbol.DoubleLeftBracket) && s.length > 0;
+}
--- src/scanner.ts
+++ src/scanner.ts
@@ -11,6 +11,21 @@ export enum TokenKind {
     Tin,
     Identifier,
     EOF,
+    Whitespace,
+    Bad
+}
+
+export enum Symbol {
+    LeftBracket = '[',
+    DoubleLeftBracket = '[[',
+    RightBracket = ']',
+    DoubleRightBracket = ']]',
+    Tilde = '~',
+    Colon = ':',
+    Comma = ',',
+    If = 'if',
+    Else = 'else',
+    Tin = 'tin'
 }
 
 export class Token {
@@ -29,20 +44,24 @@ export class Token {
     }
 }
 
+export function scan(source: string): Token[] {
+    return new Scanner(source).getTokens();
+}
+
 class Scanner {
     private tokens: Token[] = [];
     private i: number = 0;          // Current index
     private iToken: number = 0;     // Start of current token
     private iChar: number = 0;      // Index from last new line
-    private line: number = 1;       // Current line
+    private line: number = 0;       // Current line
 
-    private static keywords: Map<string, TokenKind>;
-
-    static {
-        this.keywords.set('if', TokenKind.If);
-        this.keywords.set('else', TokenKind.Else);
-        this.keywords.set('tin', TokenKind.Tin);
-    }
+    private static keywords: Map<string, TokenKind> = new Map<string, TokenKind>(
+        [
+            [Symbol.If, TokenKind.If],
+            [Symbol.Else, TokenKind.Else],
+            [Symbol.Tin, TokenKind.Tin]
+        ]
+    );
 
     constructor(
         private source: string
@@ -53,42 +72,66 @@ class Scanner {
         return this.tokens;
     }
 
+    /**
+     * Scan all tokens in the source.
+     * 
+     * @remarks
+     * Resulting tokens always start with an empty text token and end with a
+     * text token then an eof token. 
+     */
     private scanAll() {
         this.addToken(TokenKind.Text);
         while (!this.eof()) {
-            if (this.peek() == '[' && this.peek(1) == '[') {
-                this.addToken(TokenKind.Text);
+            if (
+                this.next() == Symbol.LeftBracket && 
+                this.match(Symbol.LeftBracket)
+            ) {
+                // Add text token up to tag open.
+                this.addToken(TokenKind.Text, 2);
+
+                this.addToken(TokenKind.TagOpen);
                 this.scanTag();
             }
-            this.next();
         }
         this.addToken(TokenKind.Text);
         this.addToken(TokenKind.EOF);
     }
 
+    /**
+     * Scan all tokens in a single tag.
+     * 
+     * @remarks
+     * This should be called after a tag open has already been scanned.
+     */
     private scanTag() {
         while (!this.eof()) {
+            let foundWhitespace = false;
+            while (this.next(Scanner.isWhitespace)) foundWhitespace = true;
+            if (foundWhitespace) this.addToken(TokenKind.Whitespace);
+
             switch (this.next()) {
-                case '~':
+                case Symbol.Tilde:
                     this.addToken(TokenKind.Tilde);
                     break;
-                case ':':
+                case Symbol.Colon:
                     this.addToken(TokenKind.Colon);
                     break;
-                case ',':
+                case Symbol.Comma:
                     this.addToken(TokenKind.Comma);
                     break;
-                case '[':
-                    if (this.match('[')) this.addToken(TokenKind.TagOpen);
-                    else this.error("Expected '['");
+                case Symbol.LeftBracket:
+                    this.error('Left brackets should not appear inside a ' + 
+                        'tag. (Nested tags are not allowed.)');
                     break;
-                case ']':
+                case Symbol.RightBracket:
                     if (this.match(']')) {
                         this.addToken(TokenKind.TagClose);
                         return;
                     }
-                    else this.error("Expected ']'");
-                    break;
+                    else {
+                        this.error("Expected ']'");
+                        break;
+                    }
                 default:
                     if (this.next(Scanner.isAlpha)) this.scanName();
                     else this.error("Unexpected character");
@@ -105,10 +148,20 @@ class Scanner {
         else this.addToken(TokenKind.Identifier);
     }
 
+    /**
+     * Advance the index if the next character matches `char`. 
+     * 
+     * @returns true iff the characters match.
+     */
     private match(char: string): boolean {
         return this.next((c: string) => c == char) !== null;
     }
 
+    /**
+     * Advance the index if a given predicate holds on the next character.
+     * 
+     * @returns the character read if the predicate held. Otherwise, `null`.
+     */
     private next(
         predicate: (c: string) => boolean = (_: string) => true
     ): string | null {
@@ -121,27 +174,42 @@ class Scanner {
         return matches? char : null;
     }
 
-    private eof(): boolean {
-        return this.i >= this.source.length;
+    private eof(ahead: number = 0): boolean {
+        return this.i + ahead >= this.source.length;
     } 
 
     private peek(ahead: number = 0): string {
-        if (this.eof()) return '\0';
+        if (this.eof(ahead)) return '\0';
         return this.source.charAt(this.i + ahead);
     }
 
-    private addToken(kind: TokenKind) {
+    /**
+     * Add a token whose lexeme is the substring from the last token 
+     * (exclusive) up to the current index (exclusive), possibly ignoring some
+     * trailing characters.
+     * 
+     * @param kind - The kind of token to add
+     * @param ignoreCount - The number of trailing characters to ignore
+     * 
+     * @remarks
+     * Assume that there is no new line within `ignoreCount` behind the 
+     * current index.
+     */
+    private addToken(kind: TokenKind, ignoreCount: number = 0) {
+        let iEnd = Math.max(this.i - ignoreCount, this.iToken);
+        let lexeme = this.source.substring(this.iToken, iEnd);
         this.tokens.push(new Token(
-            this.source.substring(this.iToken, this.i), 
+            lexeme, 
             kind, 
             this.line, 
-            this.iChar
+            this.iChar - (this.i - iEnd)
         ));
-        this.iToken = this.i;
+        this.iToken = iEnd;
     }
 
     private error(message: string) {
-        console.log(message + ' at ' + this.line + ':' + this.iChar + '.');
+        this.addToken(TokenKind.Bad);
+        //console.log(message + ' at ' + this.line + ':' + this.iChar + '.');
     }
 
     private static isAlpha(c: string) {
@@ -151,8 +219,8 @@ class Scanner {
     private static isAlphaNumeric(c: string) {
         return /^[a-z0-9]*$/i.test(c);
     }
-}
 
-export function scan(source: string): Token[] {
-    return new Scanner(source).getTokens();
+    private static isWhitespace(c: string) {
+        return /^[ \n\r]/.test(c);
+    }
 }
