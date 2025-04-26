aea9b70b37117ed5eac91a9afcf855132cd038d2
cwurst <cwurst@uoregon.edu>
2025-04-26 15:49:24 -0700

    Add simple syntax error reporting

--- src/features/parsing/errorReporter.ts
+++ src/features/parsing/errorReporter.ts
@@ -4,24 +4,23 @@ import * as syn from './syntaxTree';
  * Report syntax errors encountered during the parsing of a syntax tree.
  */
 export default function reportErrors(root: syn.SyntaxTree): string {
-    return 'TODO';
+    const reporter = new ErrorReporter();
+    root.accept(reporter);
+    return reporter.result;
 }
 
-class ErrorReporter extends syn.Visitor {
-    public visitTinDoc(document: syn.TinDoc): void {
+/* If we discover later that error reporting should branch differently than
+parsing recovery, we can reimplement this class as a `syn.Visitor` and treat
+each node based on its concrete type. */
+class ErrorReporter extends syn.UniformVisitor {
+    public result: string = '';
 
+    /** @override */
+    protected visit(node: syn.SyntaxTree): void {
+        const errors = node.errors;
+        for (let i = 0; i < errors.length; i++) {
+            this.result += errors[i].message + '\n';
+        }
+        node.acceptToChildren(this);
     }
-
-    public visitTextExpr(textExpr: syn.TextExpr): void {
-
-    }
-
-    public visitEOF(eof: syn.EOF): void {
-
-    }
-
-    public visitVariableTag(variableTag: syn.VariableTag): void {
-
-    }
-
 }
