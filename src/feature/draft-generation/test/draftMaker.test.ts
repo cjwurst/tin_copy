0401c1c2f205ed1accfb197d802229a135a369a0
cwurst <cwurst@uoregon.edu>
2025-05-09 11:53:59 -0700

    Add draft maker test suite skeleton

---
+++ src/feature/draft-generation/test/draftMaker.test.ts
@@ -0,0 +1,26 @@
+import { fc, test } from '@fast-check/vitest';
+import { describe, expect } from 'vitest';
+import { wellFormedParseArb, ParseResult } from '../../../common/test/syntaxArbs';
+
+// The draft maker maps (root, context) => draft.
+describe('Draft maker', () => {
+    test.todo('should leave a plaintext template unchanged', () => {
+
+    });
+
+    test.todo('should generate a draft given a full context', () => {
+
+    });
+
+    test.todo('should fail given an incomplete context', () => {
+
+    });
+
+    test.todo('should fill number variables', () => {
+
+    });
+
+    test.todo('should fill string variables', () => {
+
+    });
+});
