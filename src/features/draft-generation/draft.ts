dd384541f5877afb24eaa4ad47aebcd2e212a33f
cwurst <cwurst@uoregon.edu>
2025-05-27 12:12:43 -0700

    Add draft TinError

--- src/features/draft-generation/draft.ts
+++ src/features/draft-generation/draft.ts
@@ -1,14 +1,12 @@
+import { TinError } from '../../common/tinErrors.ts';
+
 export type Draft = {
     readonly content: string,
-    readonly errors: readonly DraftError[]
+    readonly errors: readonly TinError[]
 }
 
 export namespace Draft {
     export function isGood(draft: Draft): boolean {
         return draft.errors.length === 0;
     }
-}
-
-export type DraftError = {
-    message: string
 }
