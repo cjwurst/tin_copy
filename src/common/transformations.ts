ac41519682afa284442699a6511c48a6d937533d
cwurst <cwurst@uoregon.edu>
2025-05-30 15:35:28 -0700

    Add `fold` to transformations.ts

--- src/common/transformations.ts
+++ src/common/transformations.ts
@@ -3,3 +3,5 @@ export { parse } from '../features/parsing/parser.ts';
 export * from '../features/form-generation/formMaker.tsx';
 export * from '../features/draft-generation/draftMaker.ts';
 export * from '../features/var-declaration/tinContextMaker.ts';
+// TODO: Should this be somewhere else?
+export { fold } from '../features/parsing/syntaxTree.ts';
