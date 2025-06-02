38f221d324973b2269b0317a662cdd6d32c5613e
cwurst <cwurst@uoregon.edu>
2025-06-02 14:07:26 -0700

    Change context maker to simple function

--- src/features/var-declaration/tinContextMaker.ts
+++ src/features/var-declaration/tinContextMaker.ts
@@ -1,33 +1,26 @@
 import { TinContext, TinValue } from './tinContext.ts';
 import { SyntaxTree } from '../../common/intermediates.ts';
-import { UniformVisitor } from '../../common/visitor.ts';
 import { TinVariableError } from '../../common/tinErrors.ts';
 
 export function makeTinContext(root: SyntaxTree): TinContext {
-    const maker = new TinContextMaker();
-    maker.visit(root);
-    return maker.context;
+    const context = new Map<string, TinValue>();
+    addToContext(root, context)
+    return context;
 }
 
-// TODO: Change this to type UniformVisitor<TinContext>?
-class TinContextMaker extends UniformVisitor<void> {
-    public readonly context: TinContext = new Map<string, TinValue>();
-
-    /** @override */
-    public visit(node: SyntaxTree): void {
-        if (node.kind == 'variableTag' && node.identifier) {
-            if (TinContext.tryGet(this.context, node.identifier.lexeme)) {
-                node.errors.push(TinVariableError.make(
-                    node.identifier, 
-                    `Variable ${node.identifier.lexeme} was already declared, but 
-                        its declared again here.`
-                ));
-            } else {
-                this.context.set(node.identifier.lexeme, TinValue.make(undefined));
-            }
+function addToContext(node: SyntaxTree, context: TinContext): void {
+    if (node.kind == 'variableTag' && node.identifier) {
+        if (TinContext.tryGet(context, node.identifier.lexeme)) {
+            node.errors.push(TinVariableError.make(
+                node.identifier, 
+                `Variable ${node.identifier.lexeme} was already declared, but 
+                    its declared again here.`
+            ));
+        } else {
+            context.set(node.identifier.lexeme, TinValue.make(undefined));
         }
-        for(let i = 0; i < node.children.length; i++) {
-            this.visit(node.children[i]);
-        }
+    }
+    for(let i = 0; i < node.children.length; i++) {
+        addToContext(node.children[i], context);
     }
 }
