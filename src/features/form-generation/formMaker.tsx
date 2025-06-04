a3743f0c4e7398f0cb1cceb3baf46a4755d6a4fa
cwurst <cwurst@uoregon.edu>
2025-06-04 11:46:58 -0700

    Change form maker to simple function

--- src/features/form-generation/formMaker.tsx
+++ src/features/form-generation/formMaker.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import * as syn from '../../common/syntaxTree.ts';
 import { TinContext, TinValue } from '../var-declaration/tinContext.ts';
-import { PiecewiseVisitor } from '../../common/visitor';
 import * as assert from '../../common/staticAssert';
+
+type VarSetter = (s: string, v: TinValue) => void;
 
 /**
  * Make a form (or part of a form) from a syntax tree root (or other node).
@@ -10,78 +11,87 @@ import * as assert from '../../common/staticAssert';
 export function makeForm(
     root: syn.SyntaxTree, 
     context: TinContext, 
-    setVariable: (s: string, v: TinValue) => void
+    setVariable: VarSetter
 ): React.ReactNode {
-    return new FormMaker(context, setVariable).visit(root);
+    switch(root.kind) {
+        case 'tinDoc':
+            return makeTinDocEntry(root, context, setVariable);
+        case 'textExpr':
+            return makeTextExprEntry(root, context, setVariable);
+        case 'variableTag':
+            return 
+        case 'eof':
+            return <>
+                End of form!
+            </>;
+        default:
+            assert.isNever(root);
+    }
 }
 
-class FormMaker extends PiecewiseVisitor<React.ReactNode> {
-    constructor(
-        private readonly context: TinContext,
-        private readonly setVariable: (s: string, v: TinValue) => void
-    ) {
-        super();
-    }
+function makeTinDocEntry(
+    tinDoc: syn.TinDoc,
+    context: TinContext, 
+    setVariable: VarSetter
+): React.ReactNode {
+    return <>
+        Start of form!
+        { makeTextExprEntry(tinDoc.content, context, setVariable) }
+        { makeForm(tinDoc.eof, context, setVariable) }
+    </>;
+}
 
-    /** @override */
-    public visitTinDoc(document: syn.TinDoc): React.ReactNode {
-        return <>
-            Start of form!
-            { this.visitTextExpr(document.content) }
-            { this.visitEOF(document.eof) }
-        </>;
+function makeTextExprEntry(
+    textExpr: syn.TextExpr,
+    context: TinContext, 
+    setVariable: VarSetter
+): React.ReactNode {
+    const content = textExpr.content;
+    let input: React.ReactNode = <></>;
+    switch (content.kind) {
+        case 'string':
+            break;
+        case 'variable':
+            input = makeVariableTagEntry(content.payload, context, setVariable);
+            break;
+        default:
+            assert.isNever(content);
     }
-
-    /** @override */
-    public visitTextExpr(textExpr: syn.TextExpr): React.ReactNode {
-        const content = textExpr.content;
-        let input: React.ReactNode = <></>;
-        switch (content.kind) {
-            case 'string':
-                break;
-            case 'variable':
-                input = this.visitVariableTag(content.payload);
-                break;
-            default:
-                assert.isNever(content);
+    return <> 
+        { input }
+        { textExpr.tail? 
+            makeTextExprEntry(textExpr.tail, context, setVariable) : 
+            <></> 
         }
-        return <> 
-            { input }
-            { textExpr.tail? this.visitTextExpr(textExpr.tail) : <></> }
-        </>;
-    }
+    </>;
+}
 
-    /** @override */
-    public visitEOF(_: syn.EOF): React.ReactNode {
-        return <>
-            End of form!
-        </>;
-    }
-
-    /** @override */
-    public visitVariableTag(variableTag: syn.VariableTag): React.ReactNode {
-        if (!syn.isGood(variableTag) || !variableTag.identifier) return <></>;
+function makeVariableTagEntry(
+    variableTag: syn.VariableTag,
+    context: TinContext, 
+    setVariable: VarSetter
+): React.ReactNode {
+    if (!syn.isGood(variableTag) || !variableTag.identifier) return <></>;
         
-        const name = variableTag.identifier.lexeme;
-        const value = TinContext.tryGet(this.context, name);
-        if (!value) throw new Error(`Context does not contain identifier "name".`);
-        switch (value.kind) {
-            case 'string':
-                return <input 
-                    type='text' 
-                    // TODO: Convert from lower camel case to spaces?
-                    placeholder={name}
-                    value={value.content}
-                    onChange={e => { 
-                        this.setVariable(name, TinValue.make(e.target.value));
-                    }}
-                />
-            case 'number':
-                return <>TODO</>; // TODO
-            case 'boolean':
-                return <>TODO</>; // TODO
-            default:
-                assert.isNever(value);
-        }
+    const name = variableTag.identifier.lexeme;
+    const value = TinContext.tryGet(context, name);
+    if (!value) throw new Error(`Context does not contain identifier "name".`);
+    switch (value.kind) {
+        case 'string':
+            return <input 
+                type='text' 
+                // TODO: Convert from lower camel case to spaces?
+                placeholder={name}
+                value={value.content}
+                onChange={e => { 
+                    setVariable(name, TinValue.make(e.target.value));
+                }}
+            />
+        case 'number':
+            return <>TODO</>; // TODO
+        case 'boolean':
+            return <>TODO</>; // TODO
+        default:
+            assert.isNever(value);
     }
 }
