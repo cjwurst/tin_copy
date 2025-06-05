6c4c67794cc5f08961e3ab94a9b6a5892aa346bd
cwurst <cwurst@uoregon.edu>
2025-06-05 12:58:24 -0700

    Add OAuth2 verification

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 
-import { Token } from '../common/intermediates.ts';
-import { scan, parse } from '../common/transformations.ts';
-import Form from './Form.tsx';
+import React from 'react';
+//import Form from './Form.tsx';
 
-export default function App() {
-    const tokens: Token[] = scan("Here's some text with a [[variableTag]] and an [[otherVariableTag]].");
-    const root = parse(tokens);
-    return <Form root={root}/>;
+export default function App(): React.ReactNode {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+        console.log(token);
+    });
+    return <>Hiya, World.</>
+    //return <Form root={root}/>;
 }
